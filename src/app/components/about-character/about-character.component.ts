c8f5d384dfee35429c60743acccf9cb74d9bbc11
andrei.cruz <andrei_cruz97@hotmail.com>
2020-01-19 20:43:39 -0300

    new method lyfecicle

--- src/app/components/about-character/about-character.component.ts
+++ src/app/components/about-character/about-character.component.ts
@@ -1,26 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit } from '@angular/core';
 import { CharactersService } from 'src/app/services/characters.service';
-import { Character } from 'src/app/models/models';
+import { Character } from 'src/app/models/character';
+import { StoriesService } from 'src/app/services/stories.service';
+import { Storie } from 'src/app/models/stories';
 
 @Component({
   selector: 'app-about-character',
   templateUrl: './about-character.component.html',
   styleUrls: ['./about-character.component.sass']
 })
-export class AboutCharacterComponent implements OnInit {
+export class AboutCharacterComponent implements OnInit, AfterContentInit {
 
   private character: Character;
+  private stories: Storie[] = [];
   
-  constructor(private charactersService: CharactersService) { }
+  constructor(private charactersService: CharactersService, private storieService: StoriesService) { }
 
   ngOnInit() {
+    // this.stories = [];
     this.getData();
   }
 
+  ngAfterContentInit() {
+    console.log(this.stories);
+    
+  }
+
   getData() {
+    // this.stories = []
     this.charactersService.getData().subscribe(response => {
-      this.character = response;
-      console.log(this.character)
+      this.character = response[0];
+      response[0].stories.items.forEach(item => {
+        this.storieService.getData(item.resourceURI).subscribe(storie => {
+          this.stories.push(storie[0]);
+        })
+      })
     })
   }
 
