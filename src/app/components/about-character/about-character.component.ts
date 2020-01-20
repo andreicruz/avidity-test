e8a77a78dde8a8bb4ed7ae1efcf655d3ea4affd9
andrei.cruz <andrei_cruz97@hotmail.com>
2020-01-20 11:28:03 -0300

    changes

--- src/app/components/about-character/about-character.component.ts
+++ src/app/components/about-character/about-character.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterContentInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CharactersService } from 'src/app/services/characters.service';
 import { Character } from 'src/app/models/character';
 import { StoriesService } from 'src/app/services/stories.service';
@@ -9,10 +9,11 @@ import { Storie } from 'src/app/models/stories';
   templateUrl: './about-character.component.html',
   styleUrls: ['./about-character.component.sass']
 })
-export class AboutCharacterComponent implements OnInit, AfterContentInit {
+export class AboutCharacterComponent implements OnInit {
 
   private character: Character;
   private stories: Storie[] = [];
+  private characterOfHistories: Character[] = [];
   
   constructor(private charactersService: CharactersService, private storieService: StoriesService) { }
 
@@ -20,13 +21,16 @@ export class AboutCharacterComponent implements OnInit, AfterContentInit {
     this.getData();
   }
 
-  ngAfterContentInit() {
-    console.log(this.stories);
-    
+  getCharacter(storie: Storie) {
+    storie.characters.items.forEach(item => {
+      this.charactersService.getData(item.resourceURI.split('characters/')[1]).subscribe(character => {
+        this.characterOfHistories.push(character[0]);
+      })
+    })
   }
 
   getData() {
-    this.charactersService.getData().subscribe(response => {
+    this.charactersService.getData(1010338).subscribe(response => {
       this.character = response[0];
       response[0].stories.items.forEach(item => {
         this.storieService.getData(item.resourceURI).subscribe(storie => {
