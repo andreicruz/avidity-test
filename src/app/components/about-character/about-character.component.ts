906e77a5e3de0d9efb50120fa3c03b295b30fc86
Andrei Cruz <andrei_cruz@terceiros.sicredi.com.br>
2020-01-22 19:53:50 -0300

    methods changed

--- src/app/components/about-character/about-character.component.ts
+++ src/app/components/about-character/about-character.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CharactersService } from 'src/app/services/characters.service';
-import { Character } from 'src/app/models/character';
+import { MyObject, Results } from 'src/app/models/character';
 import { StoriesService } from 'src/app/services/stories.service';
 import { Storie } from 'src/app/models/stories';
 
@@ -11,9 +11,9 @@ import { Storie } from 'src/app/models/stories';
 })
 export class AboutCharacterComponent implements OnInit {
 
-  private character: Character;
-  private stories: Storie[] = [];
-  private characterOfHistories: Character[] = [];
+  private object: MyObject;
+  private stories: Storie;
+  private characterOfHistories: Results[] = [];
   
   constructor(private charactersService: CharactersService, private storieService: StoriesService) { }
 
@@ -22,20 +22,20 @@ export class AboutCharacterComponent implements OnInit {
   }
 
   getCharacter(storie: Storie) {
+    this.characterOfHistories = [];
     storie.characters.items.forEach(item => {
       this.charactersService.getData(item.resourceURI.split('characters/')[1]).subscribe(character => {
-        this.characterOfHistories.push(character[0]);
+        this.characterOfHistories.push(character.data.results[0]);
       })
     })
   }
 
   getData() {
     this.charactersService.getData(1010338).subscribe(response => {
-      this.character = response[0];
-      response[0].stories.items.forEach(item => {
-        this.storieService.getData(item.resourceURI).subscribe(storie => {
-          this.stories.push(storie[0]);
-        })
+      this.object = response;
+      console.log(response.data.results[0].stories.items[0].resourceURI);
+      this.storieService.getData(response.data.results[0].stories.items[0].resourceURI).subscribe(storie => {
+        this.stories = storie;
       })
     })
   }
