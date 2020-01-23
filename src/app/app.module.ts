ed22e04a136bb22b9461201db917fc0fb6a14443
andreicruz <andrei_cruz97@hotmail.com>
2020-01-22 22:30:40 -0300

    font awesome added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutCharacterComponent } from './components/about-character/about-character.component';
+import { ListCharactersComponent } from './components/list-characters/list-characters.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatCardModule } from '@angular/material/card';
-import { ListCharactersComponent } from './components/list-characters/list-characters.component';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -23,7 +26,9 @@ import { ListCharactersComponent } from './components/list-characters/list-chara
     HttpClientModule,
     BrowserAnimationsModule,
     MatExpansionModule,
-    MatCardModule
+    MatCardModule,
+    MatTooltipModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
